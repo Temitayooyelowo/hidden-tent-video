f1d362067e31cd27baad6811837df2909d3649e2
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2021-01-17 02:00:36 -0500

    remove res.redirect

--- routes/auth.js
+++ routes/auth.js
@@ -10,16 +10,16 @@ router.post('/register', (req,res) => {
   User.register({username: req.body.username, videoRatings: []}, req.body.password, (err,user) => {
       if(err){
           console.log(err);
-          // return res.status(401).send({
-          //   message: 'User unauthorized or credentials mismatch',
-          // });
-          res.redirect('/auth/register');
+          return res.status(401).send({
+            message: 'User unauthorized or credentials mismatch',
+          });
+          // res.redirect('/auth/register');
       }else{
           passport.authenticate('local')(req,res, () => {
-            // return res.status(200).send({
-            //   message: 'Authentication Successful',
-            // });
-            res.redirect('/')
+            return res.status(200).send({
+              message: 'Authentication Successful',
+            });
+            // res.redirect('/')
           });
       }
   });
@@ -40,41 +40,41 @@ router.post('/login', (req,res) => {
       }else {
           console.log('success');
           passport.authenticate('local')(req,res, () => {
-              res.redirect('/');
+              // res.redirect('/');
               // console.log("Corrrect authentication");
-              // return res.status(200).send({
-              //   message: 'Authentication Successful',
-              // });
+              return res.status(200).send({
+                message: 'Authentication Successful',
+              });
           });
       }
   });
 });
 
-// /* LOGIN ROUTER */
-router.get('/login',function(req, res, next) {
-  console.log("In login router");
-  if (req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  res.sendFile(path.join(__dirname, '../public', 'login.html'));
-});
+// // /* LOGIN ROUTER */
+// router.get('/login',function(req, res, next) {
+//   console.log("In login router");
+//   if (req.isAuthenticated()) {
+//     return res.redirect('/');
+//   }
+//   res.sendFile(path.join(__dirname, '../public', 'login.html'));
+// });
 
-router.get('/register',function(req, res, next) {
-  console.log("In register router");
-  if (req.isAuthenticated()) {
-    return res.redirect('/');
-    //res.status(400).send('Registration invalid...');
-  }
-  res.sendFile(path.join(__dirname, '../public', 'register.html'));
-});
+// router.get('/register',function(req, res, next) {
+//   console.log("In register router");
+//   if (req.isAuthenticated()) {
+//     return res.redirect('/');
+//     //res.status(400).send('Registration invalid...');
+//   }
+//   res.sendFile(path.join(__dirname, '../public', 'register.html'));
+// });
 
-router.get('/logout2',function(req, res, next) {
-  console.log("In logout router");
-  if (!req.isAuthenticated()) {
-    return res.redirect('/auth/login');
-  }
-  res.sendFile(path.join(__dirname, '../public', 'logout.html'));
-});
+// router.get('/logout2',function(req, res, next) {
+//   console.log("In logout router");
+//   if (!req.isAuthenticated()) {
+//     return res.redirect('/auth/login');
+//   }
+//   res.sendFile(path.join(__dirname, '../public', 'logout.html'));
+// });
 
 /* LOGOUT ROUTER */
 router.get('/logout', function(req, res){
